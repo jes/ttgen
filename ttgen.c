4068fa2a6a26b19122f8b26ad736e4c344e887e6
James Stanley <james@incoherency.co.uk>
2010-10-08 15:47:02 +0100

    Lexer works

--- ttgen.c
+++ ttgen.c
@@ -26,11 +26,6 @@ typedef struct Node {
   struct Node *child[2];/* pointers to child nodes */
 } Node;
 
-typedef struct Token {
-  char *text;/* the actual text of the token */
-  char type;/* VARIABLE, OPERATOR, BRACKET, or NOT */
-} Token;
-
 /* array of operator names */
 static char *operator[] = { "OR", "AND", "XOR", "NAND", "NOR", "IMP", NULL };
 
@@ -39,10 +34,6 @@ static char *operator[] = { "OR", "AND", "XOR", "NAND", "NOR", "IMP", NULL };
 #define VAR_MAX 64
 static char *variable[VAR_MAX];
 static int num_vars;
-
-/* store raw text input expressions */
-static char input[1024];
-static char *input_ptr;
 
 /* print the given message to stderr and exit with code 1 */
 static void die(const char *fmt, ...) {
@@ -79,85 +70,6 @@ static int oper_id(const char *oper_name) {
   }
 
   return -1;
-}
-
-/* resets the token stream so that it is ready for another string and clears
- * all the variables */
-static void reset_tokstr(void) {
-  int i;
-
-  input_ptr = NULL;
-
-  /* clear out the variables */
-  for(i = 0; i < num_vars; i++) {
-    free(variable[i]);
-    variable[i] = NULL;
-  }
-  num_vars = 0;
-}
-
-/* return the next token available in the global string "input", or NULL if
- * the end of the string is reached. The returned token should be free'd with
- * free_token() */
-static Token *next_token(void) {
-  Token *t;
-  size_t len;
-  int i;
-
-  /* starting a new string */
-  if(!input_ptr) input_ptr = input;
-
-  /* eat whitespace */
-  input_ptr += strspn(input_ptr, WHITESPACE);
-
-  /* reached the end of the string? */
-  if(!*input_ptr) {
-    input_ptr = NULL;
-    return NULL;
-  }
-
-  t = malloc(sizeof(Token));
-
-  /* check for brackets */
-  if(*input_ptr == '(' || *input_ptr == ')') {
-    t->text = malloc(2);
-    t->text[0] = *input_ptr;
-    t->text[1] = '\0';
-    t->type = BRACKET;
-    input_ptr++;
-    return t;
-  }
-
-  /* not a bracket, copy the word */
-  len = strcspn(input_ptr, WHITESPACE BRACKETS);
-  t->text = malloc(len + 1);
-  strncpy(t->text, input_ptr, len);
-  t->text[len] = '\0';
-  input_ptr += len;
-
-  /* special-case unary operator */
-  if(strcasecmp(t->text, "NOT") == 0) {
-    t->type = NOT;
-    return t;
-  }
-
-  /* check for operators */
-  for(i = 0; operator[i]; i++) {
-    if(strcasecmp(t->text, operator[i]) == 0) {
-      t->type = OPERATOR;
-      return t;
-    }
-  }
-
-  /* only remaining possibility is a variable */
-  t->type = VARIABLE;
-  return t;
-}
-
-/* free's the given token, including it's text field */
-static void free_token(Token *t) {
-  free(t->text);
-  free(t);
 }
 
 /* make a zero-filled node for the expression tree */
@@ -232,86 +144,4 @@ static void print_table(Node *tree) {
     printf("  ");
     printf("%d\n", evaluate(tree, i));
   }
-}
-
-int main(int argc, char **argv) {
-  int expect = EXPR;
-  Token *t;
-  Node *tree;
-
-  while(fgets(input, 1024, stdin)) {
-    tree = make_node();
-    tree->parent = tree;/* HACK: prevent the trailing bracket from failing */
-
-    /* repeatedly read tokens and store them in the expression tree */
-    while((t = next_token())) {
-      switch(t->type) {
-        case BRACKET:
-          if(*t->text == '(') {
-            if(expect != EXPR) {
-              fprintf(stderr, "error: wasn't expecting expression, got open"
-              "paren.\n");
-              goto cleanup;
-            }
-            /* branch and descend left */
-            branch(tree);
-            tree = tree->child[LEFT];
-            expect = EXPR;
-          } else /* *t->text == ')' */ {
-            /* ascend */
-            tree = tree->parent;
-            expect = OPER;
-          }
-          break;
-
-        case VARIABLE:
-          if(expect != EXPR) {
-            fprintf(stderr, "error: wasn't expecting expression, got \"%s\".\n",
-                    t->text);
-            goto cleanup;
-          }
-          /* store variable and ascend */
-          tree->type = VARIABLE;
-          tree->id = var_id(t->text);
-          tree = tree->parent;
-          expect = OPER;
-          break;
-
-        case OPERATOR:
-          if(expect != OPER) {
-            fprintf(stderr, "error: wasn't expecting operator, got \"%s\".\n",
-                    t->text);
-            goto cleanup;
-          }
-          /* store operator and descend right */
-          tree->type = OPERATOR;
-          tree->id = oper_id(t->text);
-          tree = tree->child[RIGHT];
-          expect = EXPR;
-          break;
-
-        case NOT:
-          if(expect != EXPR) {
-            fprintf(stderr, "error: wasn't expecting expression, got \"%s\".\n",
-                    t->text);
-            goto cleanup;
-          }
-          /* toggle negation flag */
-          tree->not = !tree->not;
-          break;
-      }
-
-      free_token(t);
-    }
-
-    /* print the truth table */
-    print_table(tree);
-
-    cleanup:
-    reset_tokstr();
-    free_expr(tree);
-    printf("\n");
- }
-
-  return 0;
 }
